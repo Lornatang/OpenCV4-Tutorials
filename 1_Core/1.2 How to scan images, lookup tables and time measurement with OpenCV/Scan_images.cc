c1afa9c5bf4730a5dc276d4d6b0b504a8435ff0c
Lornatang <shiyipaisizuo@163.com>
2019-07-21 23:08:19 +0800

    Update Scan_images.cc

--- 1_Core/1.2 How to scan images, lookup tables and time measurement with OpenCV/Scan_images.cc
+++ 1_Core/1.2 How to scan images, lookup tables and time measurement with OpenCV/Scan_images.cc
@@ -25,9 +25,90 @@ int test_case() {
   // do something ...
   t = ((double)getTickCount() - t) / getTickFrequency();
   cout << "Times passed in seconds: " << t << endl;
+  return 0;
 }
 
-int main(int argc, char const *argv[]) {
+/** The efficient way.
+ * When it comes to performance you cannot beat the classic C style operator[]
+ * (pointer) access. Therefore, the most efficient method we can recommend
+ * for making the assignment is:
+ */
+Mat& ScanImageAndReduceC(Mat& I, const uchar* const table) {
+  // accept only char type matrices
+  CV_Assert(I.depth() == CV_8U);
+  int channels = I.channels();
+  int nRows = I.row;
+  int nCols = I.col;
+  if (I.isContinuous()) {
+    nCols *= nRows;
+    nRows = 1;
+  }
+  int i, j;
+  uchar* p;
+  for (i = 0; i < nRows; ++i) {
+    p = I.ptr<uchar>(i);
+    for (j = 0; j < nCols; ++j) {
+      p[j] = table[p[j]];
+    }
+  }
+  return I;
+}
+
+// The iterator (safe) method
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* const table) {
+  // accept only char type matrices
+  CV_Assert(I.depth() == CV_8U);
+  const int channels = I.channels();
+  switch (channels) {
+    case 1: {
+      MatIterator_<uchar> it, end;
+      for (it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
+        *it = table[*it];
+      break;
+    }
+    case 3: {
+      MatIterator_<Vec3b> it, end;
+      for (it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end; ++it) {
+        (*it)[0] = table[(*it)[0]];
+        (*it)[1] = table[(*it)[1]];
+        (*it)[2] = table[(*it)[2]];
+      }
+    }
+  }
+  return I;
+}
+
+// On-the-fly address calculation with reference returning
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table) {
+  // accept only char type matrices
+  CV_Assert(I.depth() == CV_8U);
+  const int channels = I.channels();
+  switch (channels) {
+    case 1: {
+      for (int i = 0; i < I.rows; ++i)
+        for (int j = 0; j < I.cols; ++j)
+          I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
+      break;
+    }
+    case 3: {
+      Mat_<Vec3b> _I = I;
+      for (int i = 0; i < I.rows; ++i)
+        for (int j = 0; j < I.cols; ++j) {
+          _I(i, j)[0] = table[_I(i, j)[0]];
+          _I(i, j)[1] = table[_I(i, j)[1]];
+          _I(i, j)[2] = table[_I(i, j)[2]];
+        }
+      I = _I;
+      break;
+    }
+  }
+  return I;
+}
+
+int main(int argc, char const* argv[]) {
   test_case();
+  // ScanImageAndReduceC();
+  // ScanImageAndReduceIterator();
+  // ScanImageAndReduceRandomAccess();
   return 0;
 }
