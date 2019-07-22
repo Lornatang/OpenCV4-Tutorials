2a70752eec1c91026c4854383566da2ce3abe692
Lornatang <shiyipaisizuo@163.com>
2019-07-22 12:05:56 +0800

    Update official code

--- 2_ImgProc/2.1_basic_drawing/Drawing_1.cc
+++ 2_ImgProc/2.1_basic_drawing/Drawing_1.cc
@@ -3,9 +3,9 @@
  * @brief Simple geometric drawing
  * @author OpenCV team
  */
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include "opencv4/opencv2/core.hpp"
+#include "opencv4/opencv2/imgproc.hpp"
+#include "opencv4/opencv2/highgui.hpp"
 
 #define w 400
 
--- 2_ImgProc/Morphology_2.cc
+++ 2_ImgProc/Morphology_2.cc
@@ -4,10 +4,10 @@
  * @author OpenCV team
  */
 
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
 #include <iostream>
+#include "opencv4/opencv2/highgui.hpp"
+#include "opencv4/opencv2/imgcodecs.hpp"
+#include "opencv4/opencv2/imgproc.hpp"
 
 using namespace cv;
 
@@ -23,20 +23,18 @@ int const max_kernel_size = 21;
 
 const char* window_name = "Morphology Transformations Demo";
 
-
 /** Function Headers */
-void Morphology_Operations( int, void* );
+void Morphology_Operations(int, void*);
 
 /**
  * @function main
  */
-int main( int argc, char** argv )
-{
+int main(int argc, char** argv) {
   //![load]
-  CommandLineParser parser( argc, argv, "{@input | ../data/baboon.jpg | input image}" );
-  src = imread( parser.get<String>( "@input" ), IMREAD_COLOR );
-  if (src.empty())
-  {
+  CommandLineParser parser(argc, argv,
+                           "{@input | ../images/lena.jpg | input image}");
+  src = imread(parser.get<String>("@input"), IMREAD_COLOR);
+  if (src.empty()) {
     std::cout << "Could not open or find the image!\n" << std::endl;
     std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
     return -1;
@@ -44,30 +42,31 @@ int main( int argc, char** argv )
   //![load]
 
   //![window]
-  namedWindow( window_name, WINDOW_AUTOSIZE ); // Create window
+  namedWindow(window_name, WINDOW_AUTOSIZE);  // Create window
   //![window]
 
   //![create_trackbar1]
   /// Create Trackbar to select Morphology operation
-  createTrackbar("Operator:\n 0: Opening - 1: Closing  \n 2: Gradient - 3: Top Hat \n 4: Black Hat", window_name, &morph_operator, max_operator, Morphology_Operations );
+  createTrackbar(
+      "Operator:\n 0: Opening - 1: Closing  \n 2: Gradient - 3: Top Hat \n 4: "
+      "Black Hat",
+      window_name, &morph_operator, max_operator, Morphology_Operations);
   //![create_trackbar1]
 
   //![create_trackbar2]
   /// Create Trackbar to select kernel type
-  createTrackbar( "Element:\n 0: Rect - 1: Cross - 2: Ellipse", window_name,
-                  &morph_elem, max_elem,
-                  Morphology_Operations );
+  createTrackbar("Element:\n 0: Rect - 1: Cross - 2: Ellipse", window_name,
+                 &morph_elem, max_elem, Morphology_Operations);
   //![create_trackbar2]
 
   //![create_trackbar3]
   /// Create Trackbar to choose kernel size
-  createTrackbar( "Kernel size:\n 2n +1", window_name,
-                  &morph_size, max_kernel_size,
-                  Morphology_Operations );
+  createTrackbar("Kernel size:\n 2n +1", window_name, &morph_size,
+                 max_kernel_size, Morphology_Operations);
   //![create_trackbar3]
 
   /// Default start
-  Morphology_Operations( 0, 0 );
+  Morphology_Operations(0, 0);
 
   waitKey(0);
   return 0;
@@ -77,17 +76,18 @@ int main( int argc, char** argv )
 /**
  * @function Morphology_Operations
  */
-void Morphology_Operations( int, void* )
-{
+void Morphology_Operations(int, void*) {
   // Since MORPH_X : 2,3,4,5 and 6
   //![operation]
   int operation = morph_operator + 2;
   //![operation]
 
-  Mat element = getStructuringElement( morph_elem, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
+  Mat element = getStructuringElement(
+      morph_elem, Size(2 * morph_size + 1, 2 * morph_size + 1),
+      Point(morph_size, morph_size));
 
   /// Apply the specified morphology operation
-  morphologyEx( src, dst, operation, element );
-  imshow( window_name, dst );
+  morphologyEx(src, dst, operation, element);
+  imshow(window_name, dst);
 }
 //![morphology_operations]
